22daea4952e0dcc372ae3d1e2b0cbe6bc3e35573
Simon Sharp <simon.sharp@elca.ch>
2022-01-17 14:21:35 +0100

    Style demo components

--- dynamic-components/src/app/modules/moduleone/components/moduleone-loaded/moduleone-loaded.component.ts
+++ dynamic-components/src/app/modules/moduleone/components/moduleone-loaded/moduleone-loaded.component.ts
@@ -2,7 +2,10 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'sso-moduleone-loaded',
-  templateUrl: './moduleone-loaded.component.html'
+  templateUrl: './moduleone-loaded.component.html',
+  styleUrls: [
+    './moduleone-loaded.component.scss',
+  ],
 })
 export class ModuleoneLoadedComponent {
 }
--- dynamic-components/src/app/modules/moduletwo/pages/moduletwo-startpage/moduletwo-startpage.component.ts
+++ dynamic-components/src/app/modules/moduletwo/pages/moduletwo-startpage/moduletwo-startpage.component.ts
@@ -3,6 +3,9 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'sso-moduletwo-startpage',
   templateUrl: './moduletwo-startpage.component.html',
+  styleUrls: [
+    './moduletwo-startpage.component.scss',
+  ],
 })
 export class ModuletwoStartpageComponent {
 }
